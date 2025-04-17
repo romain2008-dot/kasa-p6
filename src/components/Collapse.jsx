dfe7520c6c23c1dfe6acc7d822090476b6c8f6ff
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-04-17 19:33:03 +0200

    ajout du style de la page error

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,19 +1,41 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import '../saas/Collapse.scss'
 import chevron from '../assets/chevron-up.svg'
 
 function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false)
+  const [contentHeight, setContentHeight] = useState('0px')
+  const contentRef = useRef(null)
+  
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(`${contentRef.current.scrollHeight}px`)
+    }
+  }, [isOpen])
+  
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <div className="collapse">
-      <button className="collapse__button" onClick={() => setIsOpen(!isOpen)}>
+      <button className="collapse__button" onClick={toggleCollapse}>
         <span className="collapse__title">{title}</span>
         <span className={`collapse__arrow ${isOpen ? 'collapse__arrow--up' : ''}`}>
           <img src={chevron} alt="chevron vers le haut" className="collapse__icon" />
         </span>
       </button>
-      {isOpen && <div className="collapse__content">{content}</div>}
+      <div 
+        className="collapse__content"
+        style={{ height: isOpen ? contentHeight : '0px' }}
+      >
+        <div 
+          ref={contentRef}
+          className={`collapse__inner ${isOpen ? 'collapse__inner--visible' : ''}`}
+        >
+          {content}
+        </div>
+      </div>
     </div>
   )
 }
